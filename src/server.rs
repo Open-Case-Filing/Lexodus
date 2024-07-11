a64e1da05f94b016276657aeec417af183605d59
tyler-harpool <tylerharpool@gmail.com>
2024-07-11 18:36:58 -0500

    Added vault support

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,18 @@
+use anyhow::Context;
 use leptos_spin::{render_best_match_to_stream, server_fn::register_explicit, RouteTable};
 use spin_sdk::http::{IncomingRequest, ResponseOutparam};
-use spin_sdk::http_component;
-
+use spin_sdk::{http_component, variables};
 #[http_component]
-async fn handle_ocfs_spin_ssr(req: IncomingRequest, resp_out: ResponseOutparam) {
+async fn handle_lexodus(req: IncomingRequest, resp_out: ResponseOutparam) {
     let mut conf = leptos::get_configuration(None).await.unwrap();
-    conf.leptos_options.output_name = "ocfs_spin_ssr".to_owned();
+    conf.leptos_options.output_name = "lexodus".to_owned();
+    let expected = variables::get("token").unwrap();
+    let expected2 = variables::get("dev_value").unwrap();
+    let response = format!(
+        "Hello, world! Token: {}, DEV_VALUE: {}",
+        expected, expected2
+    );
+    println!("Vault Response: {}", response);
 
     // Register server functions
     register_explicit::<crate::functions::save_count::SaveCount>();
